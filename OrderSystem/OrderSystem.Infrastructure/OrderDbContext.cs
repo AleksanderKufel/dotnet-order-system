c15f74b06ec98ef5e08003db736971b8ce289a04
AleksanderKufel <31322094+AleksanderKufel@users.noreply.github.com>
2026-04-25 13:07:58 +0200

    Add EF Core infrastructure with order repository

---
+++ OrderSystem/OrderSystem.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderSystem.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderSystem.Infrastructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options)
+            : base(options) { }
+    }
+}
---
+++ OrderSystem/OrderSystem.Infrastructure/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using OrderSystem.Application;
+using OrderSystem.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderSystem.Infrastructure
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderDbContext _context;
+
+        public OrderRepository(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
